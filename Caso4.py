f792cd96d5bcd93ebb8853ad8a9cac2d20fa8636
Daniella Rocha <darochap@uamv.edu.ni>
2025-05-13 10:16:13 -0600

    Caso

--- Caso4.py
+++
@@ -1,8 +0,0 @@
-#Caso 4: Registro de participación estudiantil por carrera en la UAM
-#Desarrolle un programa que registre la participación de estudiantes de la UAM en actividades
-#Derecho), cada una con tres años académicos, y cada año con dos secciones. Por cada
-#sección, se debe registrar cuántos estudiantes participaron. El programa debe mostrar el total
-#por carrera y el total general de participantes. Utilice bucles anidados.
-
-
-
