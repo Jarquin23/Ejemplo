908267345da7e0f96393ce580d2117d731c8ae7b
Rommel110207 <rommelmg@uamv.edu.ni>
2025-05-13 11:11:33 -0600

    Delete registroemanal.py

--- registroemanal.py
+++
@@ -1,7 +0,0 @@
-num_estudiantes= int(input("Ingrese la cantidad de estudiantes registrados: "))
-registro=[]
-for num_estudiantes in range(num_estudiantes):
-    nombre=input("Ingrese el nombre del estudiante: ")
-    edad=int(input("Ingrese la edad del estudiante: "))
-    carrera=input("Ingrese la carrera del estudiante: ")
-    registro.append((nombre,edad,carrera))
